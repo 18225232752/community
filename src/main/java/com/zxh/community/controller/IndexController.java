b9c37406fd184340f297f29981cddf9b33b7c29e
taehyang <taehyang@qq.com>
2023-08-28 09:21:56 +0800

    完成异常统一处理

--- src/main/java/com/zxh/community/controller/IndexController.java
+++ src/main/java/com/zxh/community/controller/IndexController.java
@@ -53,4 +53,9 @@ public class IndexController {
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
     }
+
+    @GetMapping("/error")
+    public String getErrorPage() {
+        return "/error/500";
+    }
 }
